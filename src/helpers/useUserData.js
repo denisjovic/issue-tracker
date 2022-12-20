9d936897217ccbc6a68e3f0977bac1a96bcaa551
Denis Jovic <denis.jovic@indysoft.com>
2022-12-20 08:50:28 -0500

    Add return if userId is null

--- src/helpers/useUserData.js
+++ src/helpers/useUserData.js
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 
 export function useUserData(userId) {
+  if (!userId) return;
   const userData = useQuery(["users", userId], () =>
     fetch(`/api/users/${userId}`).then((res) => res.json())
   );
